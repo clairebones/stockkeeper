83715e7c2fcfeb914f9bd61fc8b8e9e968b5cf88
Claire Wilgar <claire.wilgar@rehabstudio.com>
2016-08-21 22:20:41 +0100

    Add html loader to allow directive template files

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
     ],
     module: {
         loaders: [
+            { 
+                test: /\.html$/,
+                loader: 'html' 
+            },
             {
                 test: /\.js$/,
                 loader: 'ng-annotate',
